5ef139ac0b0cf7dfaacc9d6efa2f00221f0c54a2
HeavyLaser-Sparrow <srineeth.sana@gmail.com>
2024-09-05 17:34:33 -0700

    Update GUPPI.py

--- GUPPI.py
+++ GUPPI.py
@@ -4,19 +4,24 @@ from IPv6 import *
 
 while True:
     print("This is what you can do: ")
-    print("\t0.Exit GUPPI")
-    print("\t1.To-Do List")
-    print("\t2.Binary and IPv4 Converter")
-    print("\t3.Binary and IPv6 Converter")
+    print("\t0. Exit GUPPI")
+    print("\t1. To-Do List")
+    print("\t2. Binary and IP Converter")
+    
     whatDo = int(input("What do you want to do?: "))
-    if (whatDo == 0):
+    
+    if whatDo == 0:
         print("Exiting GUPPI")
         break
-    if(whatDo == 1):
-	    main()
-    elif(whatDo == 2):
-	    mainIPv4()
-    elif(whatDo == 3):
-        mainIPv6()
+    elif whatDo == 1:
+        main()
+    elif whatDo == 2:
+        ipChoice = int(input("Do you want to convert IPv[4] or IPv[6]? "))
+        if ipChoice == 4:
+            mainIPv4()
+        elif ipChoice == 6:
+            mainIPv6()
+        else:
+            print("Invalid IP choice.")
     else:
-	    print("I do not know what that means")
+        print("I do not know what that means")
